3d9a707d941b34edefb77a126a4cb8f6cdbad71b
Brian Lai <brianlai30@outlook.com>
2019-04-20 16:31:45 -0700

    Added prints to pandc

--- part2/pandc.c
+++ part2/pandc.c
@@ -7,6 +7,7 @@
 #include <time.h>
 #include <semaphore.h>
 
+int* buffer;
 
 /* 
  * Function to remove item.
@@ -39,5 +40,34 @@ int enqueue_item(int item) {
  *  Ctime = how long each consumer thread will sleep after consuming an item in seconds
  */
 int main(int argc, char** argv) {
+    // argv[0] is the program itself ("./pandc")
+    if (argc != 7) {
+        perror("Enter 6 arguments: \"$ ./pandc <N> <P> <C> <X> <Ptime> <Ctime>\"\n");
+        return 0;
+    }
 
+    time_t cur_time = time(0);
+    printf("Current time: %s\n", ctime(&cur_time));
+
+    int num_buffers         = strtol(argv[1], NULL, 10);
+    int num_producers       = strtol(argv[2], NULL, 10);
+    int num_consumers       = strtol(argv[3], NULL, 10);
+    int items_produced      = strtol(argv[4], NULL, 10);
+    int items_consumed      = (num_producers * items_produced) / num_consumers;
+    int over_consume        = ((num_producers * items_produced) % num_consumers > 0) ? 1 : 0;
+    int over_consume_amount = (num_producers * items_produced) - (num_consumers * items_consumed);
+    int p_time              = strtol(argv[5], NULL, 10);
+    int c_time              = strtol(argv[6], NULL, 10);
+
+    printf("\t                        Number of Buffers : %6d\n", num_buffers);
+    printf("\t                      Number of Producers : %6d\n", num_producers);
+    printf("\t                      Number of Consumers : %6d\n", num_consumers);
+    printf("\tNumber of items Produced by each producer : %6d\n", items_produced);
+    printf("\tNumber of items Consumed by each consumer : %6d\n", items_consumed);
+    printf("\t                         Over consume on? : %6s\n", (over_consume) ? "yes" : "no");
+    printf("\t                      Over consume amount : %6d\n", over_consume_amount);
+    printf("\t      Time each Producer Sleeps (seconds) : %6d\n", p_time);
+    printf("\t      Time each Consumer Sleeps (seconds) : %6d\n", c_time);
+
+    return 0;
 }
